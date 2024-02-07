6eaebd4ae8392b3650b884eb2c5f20b15ec7633c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-07 21:52:13 +0530

    Merged settings and completed the quest for proper code!

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,60 +2,63 @@
 #include <iostream>
 
 /** @brief Make sure the buffer is empty! */
-#define clear_cin() std::cin.ignore(1, '\n');
+// #define clear_cin() std::cin.ignore(); // std::numeric_limits<std::streamsize>::max(), '\n');
+#define clear_cin() std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
 int main() {
-    std::cout << "Do you know me? (Y/n)" << std::endl;
-    clear_cin();
+	std::cout << "Do you know me? (Y/n): ";
+	// Using a `switch`-statement:
+	switch (std::cin.get()) {
+		case 'Y':
+		case 'y': {
+			// Nothing here :>
+		} break;
 
-    // Using a `switch`-statement:
-    switch (std::cin.get()) {
-        case 'Y':
-        case 'y': {
-            // Nothing here :>
-        } break;
-        default: {
-            std::cout << "Wait, you don't? I'm a program!" << std::endl;
-        } break;
-    }
+		default: {
+			std::cout << "It seems you don't. Hi there, I'm a program!" << std::endl;
+		} break;
+	}
+	// Call after an `std::cin` operation:
+	clear_cin();
 
-    int uin_int = 0;
-    std::cout << "Mind entering a `" << sizeof(int) << "`-byte integer?" << std::endl;
-    clear_cin();
-    std::cin >> uin_int;
+	int uin_int = 0;
+	std::cout << "Mind entering a `" << sizeof(int) << "`-byte integer?: ";
+	std::cin >> uin_int;
+	clear_cin();
 
-    std::cout << "You mean `" << uin_int << "`?! Hey, thanks!" << std::endl;
+	std::cout << "You mean `" << uin_int << "`?! Hey, thanks!" << std::endl;
 
-    if (uin_int == 0) {
-        std::cout << "I also see that it's zero!" << std::endl;
-        goto bye_bye; // This uses a `goto`-label.
-    } else {
-        const bool below_zero = uin_int < 0;
-        std::cout << "In fact, I can count to it like so:" << std::endl;
+	if (uin_int == 0) {
+		std::cout << "I can even tell that it's zero!" << std::endl;
+		goto bye_bye; // This uses a `goto`-label.
+	} else {
+		const bool below_zero = uin_int < 0;
+		std::cout << "I can even count to it like so!: ";
 
-        const int limit = uin_int + (below_zero ? 1 : -1); // Limit for the loop.
-        // This uses another conditional - the ternary operator!
+		const int limit = uin_int + (below_zero ? 1 : -1); // Limit for the loop.
+		// This uses another conditional - the ternary operator!
 
-        int i = 0; // Iteration accumulator.
-        if (below_zero) {
-            // We're counting till an `int`, so I'll use that type here.
-            while (1) {
-                if (i < limit)
-                    break;
-                std::cout << --i << ", ";
-            }
-        } else {
-            // When counting down, we need to check the other way around!:
-            while (1) {
-                if (i > limit)
-                    break;
-                std::cout << ++i << ", ";
-            }
-        }
+		int i = 0; // Iteration accumulator.
+		if (below_zero) {
+			// We're counting till an `int`, so I'll use that type here.
+			while (1) {
+				if (i < limit)
+					break; // Showcasing `break` here.
+				std::cout << i-- << ", ";
+			}
+		} else {
+			while (1) {
+				// When counting down, we'll need to check the other way around!:
+				if (i > limit)
+					break;
+				std::cout << i++ << ", ";
+			}
+		}
 
-        std::cout << uin_int << "." << std::endl;
-    }
+		// ...A fullstop for the effect:
+		std::cout << uin_int << "." << std::endl;
+	}
 
 bye_bye: // Example of a `goto`-label!
-    std::cout << "Bye-bye!" << std::endl;
+	std::cout << "Bye-bye!" << std::endl;
 }
