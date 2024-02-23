7107d71ac6b848e1039aaa4ecadf9153f103c136
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-23 18:28:01 +0530

    Ho-ho-ho! What do we have there?!

--- src/PatternThingies/Diamond.cpp
+++ src/PatternThingies/Diamond.cpp
@@ -6,16 +6,24 @@ int main() {
     size_t max_width = 4;
 
     for (size_t i = 0; i < max_width; i++) {
+        const size_t reverse = max_width - i;
+
+        for (size_t j = 0; j <= reverse; j++)
+            std::cout << ' ';
+
+        for (size_t j = 0; j <= i; j++)
+            std::cout << '*';
+
         for (size_t j = 0; j <= i; j++)
             std::cout << '*';
 
         std::cout << std::endl;
     }
 
-    for (size_t i = max_width; i > 0; i--) {
-        for (size_t j = 0; j <= i; j++)
-            std::cout << '*';
+    // for (size_t i = max_width; i > 0; i--) {
+    //     for (size_t j = 0; j <= i; j++)
+    //         std::cout << '*';
 
-        std::cout << std::endl;
-    }
+    //     std::cout << std::endl;
+    // }
 }
