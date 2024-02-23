3b72a2180da0d161a5403432126c08a674f156f9
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-23 16:41:50 +0530

    I have half-a-diamond haha!

--- src/PatternThingies/Diamond.cpp
+++ src/PatternThingies/Diamond.cpp
@@ -6,22 +6,18 @@ int main() {
     // declare_then_get_via_cin(size_t, max_width);
     size_t max_width = 4;
 
-    for (size_t i = max_width; i > 0; i--) {
-        // We print asterisks `i + 1` times:
+    for (size_t i = 0; i < max_width; i++) {
         for (size_t j = 0; j <= i; j++)
-            std::cout << " ";
+            std::cout << '*';
 
-        std::cout << "*";
-        for (size_t j = 0; j < max_width; j++)
-            std::cout << " ";
+        std::cout << std::endl;
+    }
 
-        // for (size_t j = max_width; j > i; j--)
-        //     std::cout << " ";
+    for (size_t i = max_width; i > 0; i--) {
+        for (size_t j = 0; j <= i; j++)
+            std::cout << '*';
 
-        for (size_t j = 0; j < max_width; j++)
-            std::cout << " ";
-
-        std::cout << "*" << std::endl;
+        std::cout << std::endl;
     }
 
 }
