c6145d36914c4bc569bba42b4d30e1855ecc7162
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-02-26 16:04:58 +0530

    Uploading these haha.

---
+++ src/Switch.cpp
@@ -0,0 +1,15 @@
+#include "BrahvimIoStreamUtils.hpp"
+
+int main() {
+
+    std::cout << "Enter a number between `1` and `5`." << std::endl;
+    declare_then_get_via_cin(char, uin);
+
+    switch (uin) {
+
+        default:
+        std::cout << "" << std::endl;
+        break;
+    }
+
+}
