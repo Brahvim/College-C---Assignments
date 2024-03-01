f2134771c65335c05a149c2653ea841dbe672dbe
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-03-01 16:01:06 +0530

    Functions.
    I have less words to say. These programs are too simple to talk about.
    
    😅

---
+++ src/Functions/Functions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void printInt(int p_value) {
+    std::cout << p_value;
+}
+
+int main() {
+    printInt(5);
+}
---
+++ src/Functions/OverloadedFunctions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void print() {
+    std::cout << std::endl;
+}
+
+void print(int p_value) {
+    std::cout << p_value;
+}
+
+void print(float p_value) {
+    std::cout << p_value;
+}
+
+void print(float p_first, float p_second) {
+    std::cout << p_first << ", " << p_second;
+}
+
+int main() {
+    print(3.14f);
+    print();
+
+    print(5);
+    print();
+
+    print(3.5f, 2);
+}
