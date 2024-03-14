d29702471b1ffc37d11d89b722dde1b397892efd
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-03-14 20:11:03 +0530

    This is my way of saying "screw it!".

--- src/FileOutputStream.cpp
+++ src/FileOutputStream.cpp
@@ -5,27 +5,29 @@
 #include <tuple>
 #include <any>
 
-void exit_if_closed(const std::basic_fstream<char>& p_stream, const std::string& p_message) {
-    if (!p_stream.is_open()) {
-        std::cerr << p_message << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
+void exit_if_closed(const std::ifstream &p_stream, const std::string_view &p_message) {
+    if (p_stream.is_open())
+        return;
+
+    std::cerr << p_message << std::endl;
+    std::exit(EXIT_FAILURE);
 }
 
+void exit_if_closed(const std::ofstream &p_stream, const std::string_view &p_message) {
+    if (p_stream.is_open())
+        return;
 
-std::tuple<std::string, int> create_person() {
-    return { "Brahvim", 17 };
+    std::cerr << p_message << std::endl;
+    std::exit(EXIT_FAILURE);
 }
+
+std::tuple<std::string, int> create_person() { return { "Brahvim", 17 }; }
 
 int main() {
     std::ifstream reader("example.txt");
     std::ofstream writer("example.txt");
 
-    std::any a = 6;
-    a = 6.0f;
-    a = "";
-
-    // exit_if_closed(writer, "");
+    exit_if_closed(writer, "");
     writer << "My name is Brahvim!" << std::endl;
     writer.close();
 }
