421ba68d39c7aa1295de99c6450bf20d738179fe
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-05-04 07:56:53 +0530

    Forget there was some `std::tuple` here!

--- src/FileOutputStream.cpp
+++ src/FileOutputStream.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <tuple>
 
 void exit_if_closed(const std::ifstream &p_stream, const std::string_view &p_message) {
     if (p_stream.is_open())
@@ -18,8 +17,6 @@ void exit_if_closed(const std::ofstream &p_stream, const std::string_view &p_mes
     std::cerr << p_message << std::endl;
     std::exit(EXIT_FAILURE);
 }
-
-std::tuple<std::string, int> create_person() { return { "Brahvim", 17 }; }
 
 int main() {
     std::ifstream reader("example.txt");
